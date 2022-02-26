8fb4706f1abf07e0f70c3903c7ae0e81d70b20ed
leixiaolei <506589373@qq.com>
2022-02-26 10:01:56 +0800

    feat: add element-plus

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes = [
+import { createRouter, createWebHashHistory } from 'vue-router'
+/**
+ * 公开路由表
+ */
+const publicRoutes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    component: () => import('@/views/login/index.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHashHistory(),
+  routes: publicRoutes
 })
 
 export default router
